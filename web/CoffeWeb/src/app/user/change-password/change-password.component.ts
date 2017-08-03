38848afd1e79ac1981aa71231069f20565e438e9
Darwin Rocha <dawinrocha@gmail.com>
2017-08-03 11:47:04 -0400

    validacion de clave 2

--- web/CoffeWeb/src/app/user/change-password/change-password.component.ts
+++ web/CoffeWeb/src/app/user/change-password/change-password.component.ts
@@ -1,26 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Password } from './password.interface';
+import { BaseService } from '../../common/services/base.service';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { contentHeaders } from '../../common/headers';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.css']
 })
-export class ChangePasswordComponent implements OnInit {
-
-private passwordvalidator: Password;
-
-  constructor() { }
+export class ChangePasswordComponent  extends BaseService implements OnInit{
+  private passwordvalidator: Password;
+  private urlUser: string= this.HOST+'/user';
+  constructor(public router: Router, public http: Http) {
+      super();
+  }
 
   ngOnInit() {
     this.passwordvalidator = {
             password: '',
             confirmPassword: ''
-        }
+    }
   }
-    save(model: Password, isValid: boolean) {
+  save(model: Password, isValid: boolean) {
         // call API to save customer
         console.log(model, isValid);
-    }
+  }
 
+  changePassword(model: Password, isValid: boolean)
+  {
+     if(isValid){
+        event.preventDefault();
+        let body = JSON.stringify({model});
+        this.http.put(this.urlUser+'/reset', body,  { headers: contentHeaders })
+          .subscribe(
+              response => {
+                if (response.json().message=="Sent")
+                {
+                    alert("Le fue enviado un correo");
+                    this.router.navigate(['login']);
+                }
+
+              },
+              error => {
+                if(error.status===400)
+                {
+                  alert(error.json().message);
+                }
+                console.log(error.json().message);
+              }
+          );
+      }
+      else{alert("claves no coinciden");}
+  }
 }
