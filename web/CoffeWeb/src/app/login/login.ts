fdda3aa9a25276f1ae6baf51d65eeaf513027b6d
Darwin Rocha <dawinrocha@gmail.com>
2017-08-04 17:14:11 -0400

    solo por prueba

--- web/CoffeWeb/src/app/login/login.ts
+++ web/CoffeWeb/src/app/login/login.ts
@@ -22,6 +22,7 @@ export class Login extends BaseService{
   login(event, email, password) {
     event.preventDefault();
     let body = JSON.stringify({ email, password });
+    console.log(this.urlUser);
     this.http.post(this.urlUser+'/login', body, { headers: contentHeaders })
        .subscribe(
         response => {
