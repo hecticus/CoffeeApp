20cb7bd5002cecac00536c23bae839621290fb72
Johanna Chan <8nahchan8@gmail.com>
2017-12-05 16:52:55 -0400

    mejoras de estilo

--- web/CoffeWeb/src/app/provider/provider.component.ts
+++ web/CoffeWeb/src/app/provider/provider.component.ts
@@ -4,8 +4,8 @@ import { TableService } from '../shared/table-ns/table-ns.service';
 import { FilterService } from '../shared/filter-ns/filter-ns.service';
 
 @Component({
- 
-  	template: '<router-outlet></router-outlet>',
+
+  	template: '<router-outlet align="left" ></router-outlet>',
   	providers: [
   		TableService,
   		FilterService
