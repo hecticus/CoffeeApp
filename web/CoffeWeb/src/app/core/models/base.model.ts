0aac2e835e8ff6e79e4b9e6877e4c304a912b8a2
Shamuel Manrrique <you@example.com>
2018-06-21 16:36:30 -0400

    structure

--- web/CoffeWeb/src/app/core/models/base.model.ts
+++ web/CoffeWeb/src/app/core/models/base.model.ts
@@ -1,5 +1,5 @@
 export class BaseModel {
-	id: number;
-	createdAt: string;
-	updatedAt: string;
+    id: number;
+    createdAt: string;
+    updatedAt: string;
 }
--- web/CoffeWeb/src/app/core/models/contact.ts
+++ web/CoffeWeb/src/app/core/models/contact.ts
@@ -1,8 +1,8 @@
 export class Contact {
-	latitude: number;
-	longitude: number;
-	address: string;
-	phone: string;
-	phone2: string;
-	email2: string;
+    latitude: number;
+    longitude: number;
+    address: string;
+    phone: string;
+    phone2: string;
+    email2: string;
 }
