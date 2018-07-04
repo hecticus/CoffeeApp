f7d3801a6efb8bc11ff9ebe87828acbab1e659dc
Shamuel Manrrique <you@example.com>
2018-07-04 11:31:00 -0400

    deleted heroes proof

--- web/CoffeWeb/src/app/app.module.ts
+++ web/CoffeWeb/src/app/app.module.ts
@@ -3,11 +3,7 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './components/auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FarmComponent } from 'src/app/components/farm/farm.component';
 import { FormsModule } from '@angular/forms';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroesDetailComponent } from './components/heroes-detail/heroes-detail.component';
 import { HomeModule } from './components/home/home.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LotModule } from './components/lot/lot.module';
@@ -19,8 +15,6 @@ import { routing } from './app.routes';
 import { TableColumnDirective } from './core/table/tableColumnDirective';
 import { TableComponent } from './core/table/table.component';
 import { NotificationComponent } from './core/notification/notification.component';
-import { StatusComponent } from './components/status/status.component';
-import { ProviderComponent } from './components/provider/provider.component';
 import { InvoiceComponent } from './components/invoice/invoice.component';
 import { InvoiceDetailComponent } from './components/invoice-detail/invoice-detail.component';
 import { ProviderTypeComponent } from './components/provider-type/provider-type.component';
@@ -33,15 +27,13 @@ import { UnitComponent } from './components/unit/unit.component';
 declarations: [
 	AppComponent,
 	// ImportCore
-	HeroesComponent,
-	HeroesDetailComponent,
 
 	MessagesComponent,
-	DashboardComponent,
 	TableComponent,
 	TableColumnDirective,
 	NotificationComponent,
-	StatusComponent,
+
+
 	InvoiceComponent,
 	InvoiceDetailComponent,
 	ProviderTypeComponent,
