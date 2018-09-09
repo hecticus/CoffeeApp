ea82a855cef36fffa0c698e226986f6cba7e6a6b
shamuelmeg <shamuelmanrrique@gmail.com>
2018-09-08 21:38:59 -0400

    feat(): ready daemonTask

--- server/CoffeeApp/app/daemonTask/InvoiceDaemon.java
+++ server/CoffeeApp/app/daemonTask/InvoiceDaemon.java
@@ -1,18 +1,10 @@
 package daemonTask;
 
-import models.Invoice;
-import models.status.StatusInvoice;
-
-import java.sql.Time;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.*;
 
 public class InvoiceDaemon {
 
     public InvoiceDaemon() {
-
         changesStatus();
     }
 
@@ -28,28 +20,19 @@ public class InvoiceDaemon {
         Calendar c = Calendar.getInstance();
         c.setTime(horaDespertar);
 
-        // Si la hora es posterior a las 8am se programa la alarma para el dia siguiente
-//        if (c.get(Calendar.HOUR_OF_DAY) >= 22) {
-//            c.set(Calendar.DAY_OF_YEAR, c.get(Calendar.DAY_OF_YEAR) + 1);
-//        }
-
         c.set(Calendar.HOUR_OF_DAY, 23);
         c.set(Calendar.MINUTE, 00);
         c.set(Calendar.SECOND, 0);
 
         horaDespertar = c.getTime();
         System.out.println(horaDespertar);
-        // El despertador suena cada 24h (una vez al dia)
-        int tiempoRepeticion = 86400000;
+//        se cierran las facturas cada 24h (una vez al dia)
+//        int tiempoRepeticion = 86400000;
+        int tiempoRepeticion = 1800000;
 
-        int val = 60000;
-
-        // Programamos el despertador para que "suene" a las 8am todos los dias
+//        Programamos para que cierre las facturas a la media noche
         Timer temporizador = new Timer();
-        temporizador.schedule(new TimeClosed(), horaDespertar, val);
-
-
+        temporizador.schedule(new TimeClosed(), horaDespertar, tiempoRepeticion);
     }
-
 
 }
--- server/CoffeeApp/app/daemonTask/TimeClosed.java
+++ server/CoffeeApp/app/daemonTask/TimeClosed.java
@@ -12,53 +12,27 @@ public class TimeClosed extends TimerTask {
 
     @Override
     public void run() {
-        System.out.println("*** Loading control access tables...");
-        System.out.println("*** Loading control access tables...");
-        System.out.println("*** Loading control access tables...");
-
+        System.out.println("*** Cerrando facturas...");
+        System.out.println("*** Cerrando facturas...");
+        System.out.println("*** Cerrando facturas...");
 
         Time hora = Time.valueOf(LocalTime.now()) ;
 
         System.out.println("*** 000000000000000000000."+ hora);
 
-//        LocalDateTime todayAt6 = LocalDate.now().atTime(6, 29);
-//        Calendar calender = Calendar.getInstance();
-//        calender.set(Calendar.HOUR_OF_DAY, 24);
-//        System.out.println("hora de calender"+ calender);
-//        Date startTime = calender.getTime();
-//
-//        startTime.setHours(24);
-//        System.out.println("hora de starttime"+ startTime);
-//        Date date2pm = new java.util.Date();
-//        date2pm.setHours(14);
-//        date2pm.setMinutes(0);
-//
-//        Timer timer = new Timer();
-//        timer.schedule( new TimeClosed(), todayAt6, new Long(86400000));
-
-        /* replace with the actual task */
-        try {
-            Thread.sleep(15 * 1000);
-        } catch(InterruptedException e) {
-            e.printStackTrace();
-        }
-        /* end task processing */
-
-
         List<Invoice> invoices = Invoice.findAllInvoiceActive();
         StatusInvoice status = new StatusInvoice();
         status.setId(new Long(12));
-        if(invoices != null){
+        if(!invoices.isEmpty()){
             for (Invoice inv : invoices){
                 inv.setStatusInvoice(status);
                 inv.update();
             }
         }
-        System.out.println("00000000000");
-        System.out.println("00000000000");
-        System.out.println("00000000000");
-        System.out.println("00000000000");
-        System.out.println("00000000000");
-        System.out.println("00000000000");
+
+        System.out.println("*** Cerrando facturas...");
+        System.out.println("*** Cerrando facturas...");
+        System.out.println("*** Cerrando facturas...");
+
     }
 }
