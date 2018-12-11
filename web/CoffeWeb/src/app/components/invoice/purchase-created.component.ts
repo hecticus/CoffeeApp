c88a6329220f2a05740761c178b4bda142d486df
shamuelmeg <shamuel.manrrique@datosmeg.com>
2018-12-11 15:07:22 -0400

    fix(): add purchase

--- web/CoffeWeb/src/app/components/invoice/purchase-created.component.ts
+++ web/CoffeWeb/src/app/components/invoice/purchase-created.component.ts
@@ -7,10 +7,7 @@ import { Lot } from './../../core/models/lot';
 import { LotService } from './../lot/lot.service';
 import { InvoiceService } from './invoice.service';
 import { ItemType } from './../../core/models/item-type';
-import { Farm } from './../../core/models/farm';
-import { FarmService } from './../farm/farm.service';
 import { Location } from '@angular/common';
-import { Operacion } from 'src/app/core/models/Operacion';
 import { ProviderService } from '../provider/provider.service';
 import { FilterService } from 'src/app/core/utils/filter/filter.service';
 import { BaseService } from 'src/app/core/base.service';
@@ -21,10 +18,149 @@ import { Provider } from '@angular/compiler/src/core';
 
 @Component({
 	selector: 'app-purchase-create',
+	styleUrls: ['./invoice.component.css'],
 	template: `
-	<h2 class="title">Coffe</h2>
-			`,
-	styleUrls: ['./invoice.component.css']
+	<form *ngIf="form" [formGroup]="form"  (ngSubmit)="create()">
+		<fieldset>
+			<legend><span>Datos de la Factura</span></legend>
+			<div class="wrap-fields">
+				<div class="field form-field">
+					<mat-form-field class="example-full-width">
+						<mat-select required [formControl]="form.controls['provider']">
+							<mat-option *ngFor="let p of providers" [value]="{id: p.id}">{{p.nameProvider}}</mat-option>
+						</mat-select>
+						<mat-label><b>Proveedor</b></mat-label>
+					</mat-form-field>
+					<app-validator [control]="form.controls['provider']"></app-validator>
+				</div>
+			</div>
+
+
+			<!--<h3>new</h3> <button (click)="this.invoiceService.addHarvest()">Add Alias</button>
+				<div class="wrap-fields">
+				<div class="field form-field">
+					<mat-form-field class="example-full-width">
+						<input matInput formControlName="amountInvoiceDetail" placeholder="Cantidad" class="example-right-align">
+					</mat-form-field>
+					<app-validator [control]="form.controls['amountInvoiceDetail']"></app-validator>
+				</div>
+				</div>
+				<mat-option *ngIf="it." [value]="{id: it.id}">{{it.nameItemType}}</mat-option>
+				-->
+
+			<div formArrayName="itemtypes">
+				<div style="margin-top:5px; margin-bottom:5px;" *ngFor="let item of  itemTypesForms.controls;
+					let i=index" [formGroupName]="i">
+
+					<div class="wrap-fields">
+						<div class="field form-field">
+							<mat-form-field class="example-full-width">
+								<mat-select required [formControl]="item.controls['store']">
+									<mat-option *ngFor="let s of stores" [value]="{id: s.id}">{{f.nameStore}}</mat-option>
+								</mat-select>
+								<mat-label><b>Acopio</b></mat-label>
+							</mat-form-field>
+						</div>
+					</div>
+
+					<div class="wrap-fields">
+						<div class="field form-field">
+							<mat-form-field class="example-full-width">
+								<mat-select required [formControl]="item.controls['itemType']">
+									<mat-option *ngFor="let f of farms" [value]="{id: f.id}">{{f.nameFarm}}</mat-option>
+								</mat-select>
+								<mat-label><b>Granja</b></mat-label>
+							</mat-form-field>
+						</div>
+					</div>
+
+
+					<div class="wrap-fields">
+						<div class="field form-field">
+							<mat-form-field class="example-full-width">
+								<mat-select required [formControl]="item.controls['lot']">
+									<mat-option *ngFor="let l of lots" [value]="{id: l.id}">{{l.nameLot}}</mat-option>
+								</mat-select>
+								<mat-label><b>Lote</b></mat-label>
+							</mat-form-field>
+							<app-validator [control]="item.controls['lot']"></app-validator>
+						</div>
+					</div>
+
+					<div class="wrap-fields">
+						<div class="field form-field">
+							<mat-form-field class="example-full-width">
+								<mat-select required [formControl]="item.controls['itemType']">
+									<mat-option *ngFor="let it of itemType" [value]="{id: it.id}">{{it.nameItemType}}</mat-option>
+
+								</mat-select>
+								<mat-label><b>Grano</b></mat-label>
+							</mat-form-field>
+							<app-validator [control]="item.controls['itemType']"></app-validator>
+						</div>
+					</div>
+
+					<div class="wrap-fields">
+						<div class="field form-field">
+							<mat-form-field class="example-full-width">
+								<input matInput required formControlName="amountInvoiceDetail" placeholder="Peso" class="example-right-align">
+							</mat-form-field>
+							<app-validator [control]="item.controls['amountInvoiceDetail']"></app-validator>
+						</div>
+					</div>
+
+					<div class="wrap-fields">
+						<div class="field form-field">
+							<mat-form-field class="example-full-width">
+								<input matInput required formControlName="price" placeholder="Precio" class="example-right-align">
+							</mat-form-field>
+							<app-validator [control]="item.controls['price']"></app-validator>
+						</div>
+					</div>
+					
+					<div class="wrap-fields">
+						<div class="field">
+							<mat-form-field class="example-full-width">
+								<input matInput formControlName="nameReceived" placeholder="Recibido por">
+							</mat-form-field>
+							<app-validator [control]="item.controls['nameReceived']"></app-validator>
+						</div>
+					</div>
+
+					<div class="wrap-fields">
+						<div class="field">
+							<mat-form-field class="example-full-width">
+								<input matInput formControlName="nameDelivered" placeholder="Entregado por">
+							</mat-form-field>
+							<app-validator [control]="item.controls['nameDelivered']"></app-validator>
+						</div>
+					</div>
+					
+					<div class="wrap-fields">
+						<div class="field">
+							<mat-form-field class="example-full-width">
+								<input matInput formControlName="noteInvoiceDetail" placeholder="Observaciones">
+							</mat-form-field>
+							<app-validator [control]="item.controls['noteInvoiceDetail']"></app-validator>
+						</div>
+					</div>
+
+					<button (click)="deleteItemType(i)">Delete</button>
+
+				</div>
+
+				<button (click)="addItemType()">Add Phone Number</button>
+
+			</div>
+
+		</fieldset>
+
+		<div class="options row">
+			<button mat-raised-button class="btn-text" type="submit" >Guardar</button>
+		</div>
+	</form>
+	`
+	
 })
 
 export class PurchaseCreateComponent implements OnInit {
@@ -90,10 +226,10 @@ export class PurchaseCreateComponent implements OnInit {
 		);
 
 		let httpParamsStore = BaseService.jsonToHttpParams({
-			collection: 'id, nameFarm',
+			collection: 'id, nameStore',
 		});
 
-		this.storeService.getAll().subscribe(
+		this.storeService.getAll(httpParamsStore).subscribe(
 				data => {
 					this.stores = data['result'];
 				}
